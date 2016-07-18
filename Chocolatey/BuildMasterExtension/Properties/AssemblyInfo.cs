5d333ba897ab5f85c1247e00d91fcdc6564ad21c
Karl Harnagy <kharnagy@inedo.com>
2016-07-18 16:39:54 -0400

    Initial commit again.

---
+++ Chocolatey/BuildMasterExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using Inedo.BuildMaster.Extensibility;
+
+[assembly: AssemblyProduct("BuildMaster")]
+[assembly: RequiredBuildMasterVersion("5.0.0")]
---
+++ Chocolatey/Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Chocolatey")]
+[assembly: AssemblyDescription("Contains operations for working with Chocolatey.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2016")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
