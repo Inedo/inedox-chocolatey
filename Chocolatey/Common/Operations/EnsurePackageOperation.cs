29c8a5cb8ca9d3e941abd63261ee2cd3bf8bbd6e
Ben Lubar <ben.lubar@gmail.com>
2017-02-20 10:35:34 -0600

    Add --allow-downgrade when a specific version is requested.

--- Chocolatey/Common/Operations/EnsurePackageOperation.cs
+++ Chocolatey/Common/Operations/EnsurePackageOperation.cs
@@ -165,6 +165,7 @@ namespace Inedo.Extensions.Chocolatey.Operations
                 buffer.Append("--version \"");
                 buffer.Append(this.Template.Version);
                 buffer.Append("\" ");
+                buffer.Append("--allow-downgrade ");
             }
 
             if (!string.IsNullOrEmpty(this.Template.Source))
