65a7957fd7711ef1d69beabac8d796f715dc0732
InedoJohn <jrasch@inedo.com>
2018-02-15 11:42:59 -0500

    Reorganize

--- Chocolatey/BuildMasterExtension/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using Inedo.BuildMaster.Extensibility;
-
-[assembly: AssemblyProduct("BuildMaster")]
-[assembly: RequiredBuildMasterVersion("5.0.0")]
--- Chocolatey/InedoExtension/Properties/AssemblyInfo.cs
+++ Chocolatey/InedoExtension/Properties/AssemblyInfo.cs
@@ -1,3 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("any")]
+[assembly: AssemblyTitle("Chocolatey")]
+[assembly: AssemblyDescription("Contains operations for working with Chocolatey.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2008 - 2018")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0")]
+[assembly: CLSCompliant(false)]
